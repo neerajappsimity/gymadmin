fc39da2f2d46179c325c15fbb54735e65a1fc8c2
Neeraj Kumar <neeraj.kumar@appsimity.com>
2020-06-28 18:10:26 +0530

    Updates

---
+++ precache-manifest.280386e84628aa404092b1d7839e4c33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "165722a177c02ea0b8c9450e982e0e56",
+    "url": "/gymadmin/index.html"
+  },
+  {
+    "revision": "eb4ac682176d9ff21426",
+    "url": "/gymadmin/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "81ccd62e8dd96be029fc",
+    "url": "/gymadmin/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "eb4ac682176d9ff21426",
+    "url": "/gymadmin/static/js/2.2296bd66.chunk.js"
+  },
+  {
+    "revision": "5ab1baa52e351a54fb487f180027ee45",
+    "url": "/gymadmin/static/js/2.2296bd66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81ccd62e8dd96be029fc",
+    "url": "/gymadmin/static/js/main.1de1373d.chunk.js"
+  },
+  {
+    "revision": "0e30e8040378875164b4",
+    "url": "/gymadmin/static/js/runtime-main.e3213f35.js"
+  }
+]);
